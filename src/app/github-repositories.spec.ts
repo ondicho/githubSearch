69766c902f90b86eac3397335f9900aa09f8286f
Ondicho <ondicho53@gmail.com>
2020-08-31 04:53:55 +0300

    adds githubRepositories class

---
+++ src/app/github-repositories.spec.ts
@@ -0,0 +1,7 @@
+import { GithubRepositories } from './github-repositories';
+
+describe('GithubRepositories', () => {
+  it('should create an instance', () => {
+    expect(new GithubRepositories()).toBeTruthy();
+  });
+});
--- src/app/github-service/github.service.ts
+++ src/app/github-service/github.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment } from '../../environments/environment';
+import { Github } from '../github';
+
 
 @Injectable({
   providedIn: 'root'
