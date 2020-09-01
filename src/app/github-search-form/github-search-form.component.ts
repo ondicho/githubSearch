260e61f792dc016f8feef46a4ca2d3489a5a7604
Ondicho <ondicho53@gmail.com>
2020-09-01 22:53:01 +0300

    adds acommit to the github class

--- src/app/github-search-form/github-search-form.component.ts
+++ src/app/github-search-form/github-search-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from './../http.service';
 
 @Component({
   selector: 'app-github-search-form',
@@ -7,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubSearchFormComponent implements OnInit {
 
-  constructor() { }
+  user: any ;
+  repository: any [];
+  userName: any;
+
+  constructor(public httpService:HttpService) { }
+
+  findUser(){
+    this.httpService.updateUser(this.username);
+   
+    this.httpService.getUser().subscribe (response=>{
+     console.log (response)
+      this.user = response
+     });
+    this.httpService.getRepo().subscribe(repos=>{
+      console .log (repos)
+      this.repos = [repos]
+    });
+  
+  }
+  findRepository(){
+   this.httpService.updateUser(this.username);
+   this.httpService.getRepo().subscribe(repos=>{
+     console .log (repos)
+     this.repos = [repos]
+   });
+ 
+
 
   ngOnInit(): void {
   }
--- src/app/githubUsername.ts
+++ src/app/githubUsername.ts
@@ -1,3 +1,13 @@
 export class Github {
-  constructor(public userName:string, public repositories:string ){}
-  }
+  constructor( 
+    public username: any, 
+    public login: any,
+    public avatar :any,
+    public url:any , 
+    public followers: any,
+    public repositories: any,
+    public following: any,
+    public bio:any,
+    public email: any,
+    
+}
